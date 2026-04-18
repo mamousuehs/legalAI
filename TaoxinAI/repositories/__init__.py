b941075771199549b668991ef6b8388d82d92418
linggg00 <2848006748@qq.com>
2026-04-18 12:12:45 +0800

    填充代码
    Add repository imports and define __all__ for public API.

--- TaoxinAI/repositories/__init__.py
+++ TaoxinAI/repositories/__init__.py
@@ -1,2 +1,12 @@
 """Repository layer for TaoxinAI."""
+# app/repositories/__init__.py
 
+from .chroma_repo import ChromaRepository
+from .template_repo import TemplateRepository
+from .rule_repo import RuleRepository
+
+__all__ = [
+    "ChromaRepository",
+    "TemplateRepository",
+    "RuleRepository"
+]
